797c29ce94858d8336e9949e2191c3bb1a46a011
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-12-25 10:59:14 -0800

    Complete refactor

--- CSS/devXYZ/script.js
+++ CSS/devXYZ/script.js
@@ -3,29 +3,34 @@
 
 
 
-// Tab design from MiniTES ( https://www.kongregate.com/games/spacecowgoesmoo/minites )
-// Step 1: Create a 'hide all tabs' function
-function hideAllTabs() {
+// Tab navigation
+function displayTab(q) {
+	// Hide all tabs
 	formPage1.className = 'invisible'
 	formPage2.className = 'invisible' 
-	formPage3.className = 'invisible' 
+	formPage3.className = 'invisible'
+
+	// Display selected tab
+	window['formPage'+q].className = ''
 }
 
 
-// Step 2: onClick, hide all tabs and then show the desired tab
-let pageOneNextButtonJS = 		function() { hideAllTabs(); formPage2.className = ''; calculateZipcode(zipCodeInput.value) }
-let pageTwoBackButtonJS = 		function() { hideAllTabs(); formPage1.className = '' }
-let pageTwoNextButtonJS = 		function() { hideAllTabs(); formPage3.className = '' }
-let pageThreeBackButtonJS = 	function() { hideAllTabs(); formPage2.className = '' }
-let pageThreeSubmitButtonJS = 	function() { alert('Imaginary form submitted!'); hideAllTabs(); formPage1.className = '' }
+// Form functionality
+const searchAPI = function() { calculateZipcode(zipCodeInput.value) }
+const submitButtonAlert = function() { alert('Imaginary form submitted!') }
 
 
-// Add functions to form buttons
-pageOneNextButton.addEventListener('click', pageOneNextButtonJS, false)
-pageTwoBackButton.addEventListener('click', pageTwoBackButtonJS, false)
-pageTwoNextButton.addEventListener('click', pageTwoNextButtonJS, false)
-pageThreeBackButton.addEventListener('click', pageThreeBackButtonJS, false)
-pageThreeSubmitButton.addEventListener('click', pageThreeSubmitButtonJS, false)
+// Attach tab navigation to buttons
+pageOneNextButton.addEventListener('click', function(){displayTab(2)}, false)
+pageTwoBackButton.addEventListener('click', function(){displayTab(1)}, false)
+pageTwoNextButton.addEventListener('click', function(){displayTab(3)}, false)
+pageThreeBackButton.addEventListener('click', function(){displayTab(2)}, false)
+pageThreeSubmitButton.addEventListener('click', function(){displayTab(1)}, false)
+
+
+// Attach functionality to buttons
+pageOneNextButton.addEventListener('click', searchAPI, false)
+pageThreeSubmitButton.addEventListener('click', submitButtonAlert, false)
 
 
 
@@ -37,7 +42,7 @@ pageThreeSubmitButton.addEventListener('click', pageThreeSubmitButtonJS, false)
 // Get the Ziptastic API data
 function calculateZipcode(zipcode) {
 	if (zipcode != '') {
-		let url = 'https://ziptasticAPI.com/' + zipcode
+		const url = 'https://ziptasticAPI.com/' + zipcode
 
 		fetch(url).then(function(response) {
 			response.json().then(function(data) {
@@ -47,7 +52,7 @@ function calculateZipcode(zipcode) {
 			})
 		})
 		.catch(function(status) {
-		   	alert(status);
+		   	alert(status)
 		});
 	}
 }
