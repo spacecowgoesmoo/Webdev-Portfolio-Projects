5cd04d77b59daeef8bbc4007d0d0b19ff16d2607
Taylor Calderone <spacecowgoesmoo@gmail.com>
2022-04-12 20:43:13 -0700

    V3

--- Javascript/AES Encrypter/script.js
+++ Javascript/AES Encrypter/script.js
@@ -8,12 +8,12 @@ function encrypt(input) {
 	const cowKey1 = prompt('Please create an encryption passphrase.')
 	let cowKey2
 	if (cowKey1 != null) { cowKey2 = prompt('Please verify the encryption passphrase.') }
-	if (cowKey2 != null) {					// aka: If user didn't press the cancel button..
-		if (cowKey1 === cowKey2) {			// Check if user passphrases match
+	if (cowKey2 != null) {			// If user didn't press the cancel button..
+		if (cowKey1 === cowKey2) {	// Check if user passphrases match
 			const output = qqq.AES.encrypt(input, cowKey1)
 			return output
 		} else {
-			alert('Keys do not match, please try again.')
+			alert("Passphrases don't match, please try again.")
 			return input
 		}
 	} else return input	// user pressed the cancel button
@@ -30,14 +30,6 @@ function decryptButtonClick() {
 	const input = cowTextarea.value
 	const output = decrypt(input)
 	if (output != input) {
-		cowTextarea.value = "Decrypted text will display in 3 seconds.."
-		encryptButton.disabled = true
-		decryptButton.disabled = true
-		setTimeout(q, 3000)
-	}
-	function q() {
-		encryptButton.disabled = false
-		decryptButton.disabled = false
 		cowTextarea.value = output
 	}
 }
